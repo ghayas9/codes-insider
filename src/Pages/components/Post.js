da3c5e58f10c6843f66e16ff4889a382487c3bc4
ghayas9 <ghayasudin999@gmail.com>
2021-12-01 18:35:58 +0500

    1/12/2021

--- src/Pages/components/Post.js
+++ src/Pages/components/Post.js
@@ -1,10 +1,15 @@
 function Post(){
+    var post=[{
+        title:'Lorem, ipsum dolor sit amet consectetur Lorem, ipsum dolor sit amet consectetur ',
+        describe:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet omnis error perspiciatis libero, quod ducimus illum dolorum perferendis fugiat accusantium.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet omnis error perspiciatis libero, quod ducimus illum dolorum perferendis fugiat accusantium.',
+
+    }]
     return(
         <div className="post row">
             <img src="https://sp-ao.shortpixel.ai/client/q_lqip,ret_wait,w_500,h_360/https://codesinsider.com/wp-content/uploads/2021/11/flutter-cupertino-sliver-navigation-bar-example-tutorial-500x360.png" alt="" className="col-md-4" />
             <div className="col-md-8 d">
                 <h4 className="title">
-                    Lorem, ipsum dolor sit amet consectetur 
+                    {post[0]['title'].slice(0,54)}...
                 </h4>
                 <div className="about-post">
                 <span><i className="fas fa-user"></i> by <b>GHAYAS</b> </span>
@@ -12,7 +17,7 @@ function Post(){
                 <span><i className="far fa-comment"></i> 0</span>
                 </div>
                 <div className="post-detail">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet omnis error perspiciatis libero, quod ducimus illum dolorum perferendis fugiat accusantium.
+                {post[0]['describe'].slice(0,150)}...
                 </div>
             </div>
         </div>
